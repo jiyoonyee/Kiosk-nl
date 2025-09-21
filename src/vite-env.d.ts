a0faac687b8be95bee5c91315816dc2d53b1c93f
jiyoonyee <a01037651377@gmail.com>
2025-09-22 00:18:29 +0900

    build: 모듈 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,8 @@
 declare module "*.css";
 declare module "*.png";
 declare module "*.jpg";
+declare module "swiper/css" {}
+declare module "swiper/css/pagination" {}
+declare module "swiper/css/autoplay" {}
+declare module "swiper/css/effect-fade" {}
+declare module "swiper/css/navigation" {}
